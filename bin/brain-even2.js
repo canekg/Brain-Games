673aaaf745c0083a1f7fffbeb00722df9c88b4ce
Alexandr <canekg@mail.ru>
2023-04-11 23:01:39 +0500

    deleted: bin/brain-even2.js, src/calc.js, src/even.js, src/even2.js

--- bin/brain-even2.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-// импорт функции для приветствия
-// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
-import Game from '../src/index.js';
-// вызов функции
-Game();
