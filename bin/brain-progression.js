bd10993cce7488849860ec59c883fd1594ada4a7
Alexandr <canekg@mail.ru>
2023-04-15 21:18:02 +0500

    add game brain-progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// импорт функции для приветствия
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
+import Game from '../src/Game/progression.js';
+// вызов функции
+Game();
