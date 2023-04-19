68896f081ce570342055db15b4c966d0ae32fbed
Alexandr <canekg@mail.ru>
2023-04-19 20:23:43 +0500

    the name of the parameters has been corrected, the code for checking the response has been shortened

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const game = (task, gameoption) => {
+const game = (task, gameSettingsBuilding) => {
   // Приветствие
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log('Hello,', `${userName}!`);
   // eslint-disable-next-line no-undef
   console.log(task);
-  const maxraunds = 3;
+  const maxRaunds = 3;
   // Цикл (3 задачи - 3 ответа)
-  for (let i = 0; i < maxraunds; i += 1) {
-    const [answerchek, question] = gameoption();
+  for (let i = 0; i < maxRaunds; i += 1) {
+    const [answerCheck, question] = gameSettingsBuilding();
     // eslint-disable-next-line no-undef
     console.log('Question:', `${question}`);
     const answer = readlineSync.question('Your answer: ');
     // проверка на правильный ответ
-    if (answerchek === answer) {
-      console.log('Correct!');
-      // eslint-disable-next-line no-undef
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answerchek}'.`);
+    if (answerCheck !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answerCheck}'.`);
       console.log("Let's try again,", `${userName}!`);
       return;
     }
+    console.log('Correct!');
+    console.log('Congratulations,', `${userName}!`);
   }
-  console.log('Congratulations,', `${userName}!`);
 };
 export default game;
