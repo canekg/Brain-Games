9818da259f36758e70f61be6d9e9a738b76d0211
Alexandr <canekg@mail.ru>
2023-04-21 20:36:24 +0500

    rename func and correct func generateProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,35 +1,27 @@
-/* eslint-disable max-len */
-// импорт функции для приветствия
-import game from '../index.js';
-import randomNumberGeneration from '../utils.js';
+import playGame from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const task = 'What number is missing in the progression?';
 
-const answerCalculation = (randomElement, step, numberElements, indexElementReplacement) => {
-  let element = randomElement;
+const generateProgression = (randomElement, step, numberElements, indexElementReplacement) => {
   const newarray = [];
   for (let i = 0; i < numberElements; i += 1) {
-    if (i !== indexElementReplacement) {
-      newarray.push(element);
-    }
-    if (i === indexElementReplacement) {
-      newarray.push('..');
-    }
-    element += step;
+    const element = randomElement + i * step;
+    i !== indexElementReplacement ? newarray.push(element) : newarray.push('..');
   }
   return newarray;
 };
 
-const gameSettingsBuilding = () => {
-  const randomElement = randomNumberGeneration(1, 10);
-  const step = randomNumberGeneration(1, 10);
-  const numberElements = randomNumberGeneration(5, 10);
-  const indexElementReplacement = randomNumberGeneration(0, numberElements);
+const setGameSettings = () => {
+  const randomElement = getRandomNumber(1, 10);
+  const step = getRandomNumber(1, 10);
+  const numberElements = getRandomNumber(5, 10);
+  const indexElementReplacement = getRandomNumber(0, numberElements);
   const answerCheck = String(randomElement + step * indexElementReplacement);
-  const stringarray = answerCalculation(randomElement, step, numberElements, indexElementReplacement).join(' ');
+  const stringarray = generateProgression(randomElement, step, numberElements, indexElementReplacement).join(' ');
   const question = ('Question:', stringarray);
   return [answerCheck, question];
 };
 
-const brainProgression = () => game(task, gameSettingsBuilding);
-export default brainProgression;
+const startBrainProgression = () => playGame(task, setGameSettings);
+export default startBrainProgression;
