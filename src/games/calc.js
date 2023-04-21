9ddd42e546c6396847a414d884725b5ee8f3a5e4
Alexandr <canekg@mail.ru>
2023-04-21 20:35:53 +0500

    rename func and correct func getResultExpression

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,36 +1,31 @@
-// импорт функции для приветствия
-import game from '../index.js';
-import randomNumberGeneration from '../utils.js';
+import playGame from '../index.js';
+import getRandomNumber from '../utils.js';
 
 const task = 'What is the result of the expression?';
 
-const answerCalculation = (operator, number1, number2) => {
-  let outcome;
+const getResultExpression = (operator, number1, number2) => {
   switch (operator) {
     case '+':
-      outcome = String(number1 + number2);
-      break;
+      return number1 + number2;
     case '-':
-      outcome = String(number1 - number2);
-      break;
+      return number1 - number2;
     case '*':
-      outcome = String(number1 * number2);
-      break;
+      return number1 * number2;
     default:
       break;
   }
-  return outcome;
+  return 'error';
 };
 
-const gameSettingsBuilding = () => {
-  const randomNumber1 = randomNumberGeneration(1, 100);
-  const randomNumber2 = randomNumberGeneration(1, 100);
+const setGameSettings = () => {
+  const randomNumber1 = getRandomNumber(1, 100);
+  const randomNumber2 = getRandomNumber(1, 100);
   const operation = ['+', '-', '*'];
-  const randomOperation = operation[randomNumberGeneration(0, 3)];
+  const randomOperation = operation[getRandomNumber(0, 3)];
   const question = ('Question:', `${randomNumber1} ${randomOperation} ${randomNumber2}`);
-  const answerCheck = answerCalculation(randomOperation, randomNumber1, randomNumber2);
+  const answerCheck = String(getResultExpression(randomOperation, randomNumber1, randomNumber2));
   return [answerCheck, question];
 };
 
-const brainCalc = () => game(task, gameSettingsBuilding);
-export default brainCalc;
+const startBrainCalc = () => playGame(task, setGameSettings);
+export default startBrainCalc;
